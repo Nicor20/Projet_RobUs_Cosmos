eb6bc9a0d4f8ae0d65c88a3ab744799f47a49ffc
Nicolas <cantin.nicolas2000@gmail.com>
2020-10-11 16:20:17 -0400

    v5

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,7 @@ Inclure les librairies de functions que vous voulez utiliser
 **************************************************************************** */
 
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
-#include <Arduino.h>
-#include <stdio.h>
+
 /* ****************************************************************************
 Variables globales et defines
 **************************************************************************** */
@@ -40,36 +39,16 @@ float correctionPID(uint8_t motorSelect, uint32_t setpoint)
   float errorI = 0.00;
   float errorP = 0.00;
 
-  //Serial.println("START");
-  //Serial.println(cycleCount);
-
   if(motorSelect == LEFT)
  {
     errorI = setpoint*cycleCount;
     errorI = errorI - encoderTotal_Left;
-        //Serial.println(errorI);
-
     errorI = errorI * KI;
 
-    // Serial.println(setpoint*cycleCount);
-    // Serial.println(encoderTotal_Left);
-    // Serial.println(errorI);
-    // Serial.println("");
-  
     errorP = setpoint;
     errorP = errorP - encoderRead_Left;
-    //Serial.println(errorP);
     errorP = errorP * KP;
 
-// Serial.println(setpoint);
-// Serial.println(encoderRead_Left);
-// Serial.println(errorP);
-//     Serial.println("");
-
-
-     //Serial.println(errorP+errorI);
-
-// Serial.println("END");
     return (errorP + errorI);
   }
   else
@@ -90,22 +69,18 @@ void updateSpeed(uint32_t setpoint)
 {
   cycleCount++;
 
-  encoderRead_Left = ENCODER_ReadReset(LEFT);
+  encoderRead_Left = ENCODER_ReadReset(0);
   encoderTotal_Left += encoderRead_Left;
 
-  encoderRead_Right = ENCODER_ReadReset(RIGHT);
+  encoderRead_Right = ENCODER_ReadReset(1);
   encoderTotal_Right += encoderRead_Right;
 
-  speed_Left = speed_Left + correctionPID(LEFT, setpoint);
-  speed_Right = speed_Right + correctionPID(RIGHT, setpoint);
+  speed_Left = speed_Left + correctionPID(0, setpoint);// appelle la fonction PID
+  speed_Right = speed_Right + correctionPID(1, setpoint);// appelle la fonction PID
 
-  Serial.println(speed_Left);
-  Serial.println(speed_Right);
-
-  
-  MOTOR_SetSpeed(LEFT, speed_Left);
-  MOTOR_SetSpeed(RIGHT, speed_Right);
-
+  MOTOR_SetSpeed(0, speed_Left);
+  MOTOR_SetSpeed(1, speed_Right);
+//Pourrait probablement être enlevé.
   if(cycleCount>500)
   {
     cycleCount = 0;
@@ -114,6 +89,7 @@ void updateSpeed(uint32_t setpoint)
   }
 }
 
+//Fonction avancé. Set point sert au PID
 void robus_Avance(float vitesse, uint32_t setpoint, uint32_t distance)
 {
  
@@ -125,32 +101,88 @@ void robus_Avance(float vitesse, uint32_t setpoint, uint32_t distance)
 
   ENCODER_Reset(LEFT);
   ENCODER_Reset(RIGHT);
-
+  //Boucle qui fait avancer le robot tant que la distance parcourue ne dépasse pas la distance voulue
   while(distanceParcourue < distance)
   {
     delay(100);
-    updateSpeed(setpoint);
-    distanceParcourue += encoderTotal_Left;
-    if(distanceParcourue > 100000)
-    {
-      distanceParcourue = 0;
-    }
+    updateSpeed(setpoint);//PID
+    distanceParcourue += encoderRead_Left;
+    Serial.println(distanceParcourue);
   }
 
   MOTOR_SetSpeed(LEFT, 0);
   MOTOR_SetSpeed(RIGHT, 0);
 }
 
-void robus_Tourne(float vitesse, uint32_t setpoint, uint32_t angle)
+//Fonction tourner à gauche où on met l'angle en degré
+void robus_TourneGauche(float vitesse, float angle)
 {
-  /*
-    uint32_t angleParcouru = 0;
 
-      MOTOR_SetSpeed(LEFT, vitesse);
-      MOTOR_SetSpeed(RIGHT, -(vitesse + 0.01));
-      */
+  uint32_t angleParcouru = 0;
+  float temp;
+  angle = (angle / 360)*120.64;
+  temp = angle;
+  angle = conversionDistancePulse(temp);
+
+  MOTOR_SetSpeed(RIGHT, vitesse);
+  MOTOR_SetSpeed(LEFT, 0);
+
+  delay(50);
+
+  ENCODER_Reset(LEFT);
+  ENCODER_Reset(RIGHT);
+  while(angleParcouru < angle)
+  {
+    delay(50);//Délai diminué pour une meilleure précision
+    encoderRead_Right = ENCODER_ReadReset(RIGHT);
+    angleParcouru += encoderRead_Right;
+    Serial.println(angleParcouru);
+  }
+  MOTOR_SetSpeed(LEFT, 0);
+  MOTOR_SetSpeed(RIGHT, 0);
+
 }
 
+//Fonction pour tourner à droite avec l'angle en degré
+void robus_TourneDroite(float vitesse, float angle)
+{
+  uint32_t angleParcouru = 0;
+  float temp;
+  angle = (angle / 360)*120.64;
+  temp = angle;
+  angle = conversionDistancePulse(temp);
+
+  MOTOR_SetSpeed(LEFT, vitesse);
+  MOTOR_SetSpeed(RIGHT, 0);
+
+  delay(50);
+
+  ENCODER_Reset(LEFT);
+  ENCODER_Reset(RIGHT);
+  while(angleParcouru < angle)
+  {
+    delay(50);//Delai diminuer pour une meilleure précision
+    encoderRead_Left = ENCODER_ReadReset(0);//Pourrait être optimisé en 1 ligne
+    angleParcouru += encoderRead_Left;
+    Serial.println(angleParcouru);
+  }
+  MOTOR_SetSpeed(LEFT, 0);
+  MOTOR_SetSpeed(RIGHT, 0);
+
+}
+
+//fonctio nqui traduit une distance en cm en pulses pour que ROBUS comprenne
+uint32_t conversionDistancePulse(float centimetre)
+{
+  float distance = 0;
+
+  distance = (centimetre * 3200);
+  distance = distance / 23.939;
+
+  return (int)distance;
+}
+
+//fonction d'appellation des directions (Mrs N)
 void Lecture_Chemin(char path[])
 {
   int i=0;
@@ -162,88 +194,76 @@ void Lecture_Chemin(char path[])
   {
     if(path[i] == '/')
     { 
-      if(path[y-2] == 'a'||path[y-2] == 'd'||path[y-2] == 'g'||path[y-2] == 'r'||path[y-2] == 'x'||path[y-2] == 'y')
+      int True = 1;
+      for(a = y ; a < i ; a++)
       {
-        if(path[y-1] == ' ')
+        if(path[a] == '0'||path[a] == '1'||path[a] == '2'||path[a] == '3'||path[a] == '4'||path[a] == '5'||path[a] == '6'||path[a] == '7'||path[a] == '8'||path[a] == '9'||path[a] == '.')
         {
-          int True = 1;
-          for(a = y ; a < i ; a++)
-          {
-            if(path[a] == '0'||path[a] == '1'||path[a] == '2'||path[a] == '3'||path[a] == '4'||path[a] == '5'||path[a] == '6'||path[a] == '7'||path[a] == '8'||path[a] == '9'||path[a] == '.')
-            {
-              valeur[a-y] = path[a];
-            }
-            else
-            {
-              True = 0;
-              break;
-            } 
-          }
-          valeur[a-y] = '\0';
-
-          if(True == 1)
-          {
-            float value = (float)atof(valeur);
-
-            if(path[y-2] == 'a')
-            {
-              //Pour avancer
-              Serial.print("Avance : ");
-              Serial.println(value);
-            }
-            else if(path[y-2] == 'd')
-            {
-              //Pour tourner a droite avec une roue
-              Serial.print("Tourne a droit : ");
-              Serial.println(value);
-            }
-            else if(path[y-2] == 'g')
-            {
-              //Pour tourner a gauche avec une roue
-              Serial.print("Tourne a gauche : ");
-              Serial.println(value);
-            }
-            else if(path[y-2] == 'x')
-            {
-              //Pour tourner a droite avec deux roues
-              Serial.print("U turn a droit : ");
-              Serial.println(value);
-            }
-            else if(path[y-2] == 'y')
-            {
-              //Pour tourner a gauche avec deux roues
-              Serial.print("U turn a gauche : ");
-              Serial.println(value);
-            }
-            else
-            {  
-              //Pour reculer
-              Serial.print("Recule : ");
-              Serial.println(value);
-            }
-          }
-          else
-          {
-            Serial.println("Erreur : format valide 10 ou 10.21");
-          }
+          valeur[a-y] = path[a];
         }
         else
         {
-          Serial.println("Erreur : le second caractère n'est pas un espace");
+          True = 0;
+          break;
+        } 
+      }
+      valeur[a-y] = '\0';
+
+      if(True == 1 && path[y-1] == ' ')
+      {
+        float value = (float)atof(valeur);
+        if(path[y-2] == 'a' || path[y-2] == 'A')
+        {
+          //Pour avancer
+          Serial.print("Avance : ");
+          Serial.println(value);
+          robus_Avance(0.30, 350,conversionDistancePulse(value));
         }
+        else if(path[y-2] == 'd' || path[y-2] == 'D')
+        {
+          //Pour tourner a droite avec une roue
+          Serial.print("Tourne a droit : ");
+          Serial.println(value);
+          robus_TourneDroite(0.30, value);
+        }
+        else if(path[y-2] == 'g'||path[y-2] == 'G')
+        {
+          //Pour tourner a gauche avec une roue
+          Serial.print("Tourne a gauche : ");
+          Serial.println(value);
+          robus_TourneGauche(0.30, value);
+        }
+        else if(path[y-2] == 'r'||path[y-2] == 'R')
+        {
+          
+        }
+        else if(path[y-2] == 'x'||path[y-2] == 'X')
+        {
+          //Pour tourner a droite avec deux roues
+          Serial.print("U turn a droit : ");
+          Serial.println(value);
+        }
+        else if(path[y-2] == 'y'||path[y-2] == 'Y')
+        {
+          //Pour tourner a gauche avec deux roues
+          Serial.print("U turn a gauche : ");
+          Serial.println(value);
+        }
+        else
+        {
+          Serial.println("Erreur : ne commence pas par (a-A),(d-D),(g-G),(r-R),(x-X),(y-Y)");
+        }
+        
       }
       else
       {
-        Serial.println("Erreur : ne commence pas par a-d-g-r-x-y");
-      }    
+        Serial.println("Erreur : LA valeur n'est pas valide ou le caractere #2 n'est pas un espace");
+      } 
       y=i+3;
     }
     i++;
   }
 }
-
-
-
 
 /* ****************************************************************************
 Fonctions d'initialisation (setup)
@@ -252,8 +272,11 @@ Fonctions d'initialisation (setup)
 // -> Se fait appeler seulement un fois
 // -> Generalement on y initilise les variables globales
 
-void setup()
-{
+void setup(){
+
+  //variables
+  uint32_t i = 100000;
+
   Serial.begin(9600);
   //a = Avancer (en cm)
   //d = Tourner à droit (en °)
@@ -263,19 +286,35 @@ void setup()
   //y = uturn gauche
 
   char path[] = "a 50/d 45/g 40.5/r 160/x 45/y 45/";
-  Lecture_Chemin(path);
-
-
-  /*
+  //Lecture_Chemin(path);
   delay(1000);
   BoardInit();
   delay(1500);
 
-  robus_Avance(0.30, 350, 1000000);
+//Parcours
+  robus_Avance(0.30, 350,conversionDistancePulse(110));
+    delay(1000);
+  robus_TourneGauche(0.30, 88);
+    delay(1000);
+  robus_Avance(0.30, 350,conversionDistancePulse(68));
+    delay(1000);
+  robus_TourneDroite(0.30, 88);
+    delay(1000);
+  robus_Avance(0.30, 350,conversionDistancePulse(85));
+    delay(1000);
+  robus_TourneDroite(0.30, 39);
+    delay(1000);
+  robus_Avance(0.30, 350,conversionDistancePulse(175));
+    delay(1000);
+  robus_TourneGauche(0.30, 90);
+  delay(1000);
+ robus_Avance(0.30, 350, conversionDistancePulse(60));
+  delay(1000);
+   robus_TourneDroite(0.30, 45);
+   delay(1000);
+   robus_Avance(0.30, 350, conversionDistancePulse(95));
+  robus_TourneDroite(0.30, conversionDistancePulse(180));
 
-  MOTOR_SetSpeed(LEFT, 0.30);
-  MOTOR_SetSpeed(RIGHT, 0.309);
-  */
 }
 
 /* ****************************************************************************
